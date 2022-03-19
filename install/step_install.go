76d0fac6191b171808e37e81d8446aa150d628ec
Tom <twitchyliquid64@ciphersink.net>
2022-03-19 15:25:58 -0700

    Copy any network settings configuring during install

--- install/step_install.go
+++ install/step_install.go
@@ -1,10 +1,14 @@
 package install
 
-import "os/exec"
+import (
+	"os/exec"
+	"path/filepath"
+)
 
 type InstallStep struct{}
 
 func (s *InstallStep) Exec(updateChan chan Update, run *Run) error {
+	mountBase := "/mnt"
 	progressInfo(updateChan, "Commencing installation.\n")
 
 	e := exec.Command("sudo", "nixos-install", "--no-root-passwd")
@@ -18,6 +22,14 @@ func (s *InstallStep) Exec(updateChan chan Update, run *Run) error {
 		return err
 	}
 	e.Wait()
+
+	// Copy any network connections the user configured during installation.
+	e = exec.Command("sudo", "cp", "-rv", filepath.Join("/etc", "NetworkManager", "system-connections"), filepath.Join(mountBase, "etc", "NetworkManager"))
+	out, err := e.CombinedOutput()
+	if err != nil {
+		progressInfo(updateChan, "  Output: %q\n", string(out))
+		return err
+	}
 
 	return nil
 }
